c7eff2b0efb239dded2c3975153293ca195f809f
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2024-01-09 23:07:57 -0300

    Adding Token to API call

--- src/UserLists.js
+++ src/UserLists.js
@@ -5,8 +5,15 @@ export default class UserLists extends React.Component{
     state = { lists: null, loading: true }
 
     async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 74849150daca762d9832abed8553417c29dc81d7'
+
         var url = 'http://127.0.0.1:8000/list/';
-        const response = await fetch(url);
+        const response = await fetch(url, config);
         const data = await response.json();
         console.log(data);
         this.setState({lists: data, loading: false});
